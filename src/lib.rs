949bcb2d5ef27af91c5d32be5da4205ee1c1fe97
Xiyu Zhai <xiyuzhai95@gmail.com>
2025-11-14 20:35:53 -0800

    save

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,28 @@
 #![feature(generic_const_exprs)]
 #![allow(incomplete_features)]
-trait S { const N: usize; }
-trait C<'a> { type S: S; }
-struct M<T: S>([u8; T::N]) where [(); T::N]:;
-impl<T: S> M<T> where [(); T::N]: { fn new<'a, U>() -> Self where U: C<'a, S = T> { Self([0; T::N]) } }
-fn ice<'a, U: C<'a>>() -> M<U::S> where [(); U::S::N]: { M::new::<U>() }
+trait S {
+    const N: usize;
+}
+trait C<'a> {
+    type S: S;
+}
+struct M<T: S>([u8; T::N])
+where
+    [(); T::N]:;
+impl<T: S> M<T>
+where
+    [(); T::N]:,
+{
+    fn new<'a, U>() -> Self
+    where
+        U: C<'a, S = T>,
+    {
+        Self([0; T::N])
+    }
+}
+fn ice<'a, U: C<'a>>() -> M<U::S>
+where
+    [(); U::S::N]:,
+{
+    M::new::<U>()
+}
