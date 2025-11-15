f3cad48514709c479448438802bdddcb1e116804
Xiyu Zhai <xiyuzhai95@gmail.com>
2025-11-14 20:32:06 -0800

    save

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,17 @@
 #![feature(generic_const_exprs)]
 #![allow(incomplete_features)]
 
-trait Scheme {
-    type Node;
-    const N: usize;
-}
+trait Scheme { type Node; const N: usize; }
+trait Context<'a> { type S: Scheme; }
 
-trait Context<'a> {
-    type S: Scheme;
-}
+struct Map<S: Scheme>([S::Node; S::N]) where [(); S::N]:;
 
-struct Map<S: Scheme>
-where
-    [(); S::N]:,
-{
-    _data: [S::Node; S::N],
-}
-
-impl<S: Scheme> Map<S>
-where
-    [(); S::N]:,
-{
-    fn new<'a, C>() -> Self
-    where
-        C: Context<'a, S = S>,
-    {
-        Self {
-            _data: unsafe { std::mem::zeroed() },
-        }
+impl<S: Scheme> Map<S> where [(); S::N]: {
+    fn new<'a, C>() -> Self where C: Context<'a, S = S> {
+        Self(unsafe { std::mem::zeroed() })
     }
 }
 
-fn trigger_ice<'a, C: Context<'a>>() -> Map<C::S>
-where
-    [(); C::S::N]:,
-{
+fn trigger_ice<'a, C: Context<'a>>() -> Map<C::S> where [(); C::S::N]: {
     Map::new::<C>()
 }
