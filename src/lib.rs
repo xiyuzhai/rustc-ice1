3b12fe56d49aa995e2ae21258b6a7af33bbebb21
Xiyu Zhai <xiyuzhai95@gmail.com>
2025-11-14 20:25:56 -0800

    save

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,39 @@
-// Minimal reproduction of rustc ICE in borrow checker
-// Related to generic const parameters + complex lifetimes + associated types
-
 #![feature(generic_const_exprs)]
 #![allow(incomplete_features)]
 
-pub trait IsScheme {
+trait IsScheme {
     type Node;
     const N: usize;
 }
 
-pub trait IsContext<'db> {
+trait IsContext<'db> {
     type Scheme: IsScheme;
 }
 
-pub struct Map<S: IsScheme, V>
+struct Map<S: IsScheme>
 where
     [(); S::N]:,
 {
-    _entries: [(S::Node, V); S::N],
+    _data: [S::Node; S::N],
 }
 
-impl<S: IsScheme, V> Map<S, V>
+impl<S: IsScheme> Map<S>
 where
     [(); S::N]:,
 {
-    pub fn new<'db, C>(_ctx: C, _x: &'db ()) -> Self
+    fn new<'db, C>(_ctx: C) -> Self
     where
         C: IsContext<'db, Scheme = S>,
-        V: Default,
     {
-        let _entries = std::array::from_fn(|_| (unsafe { std::mem::zeroed() }, V::default()));
-        Self { _entries }
-    }
-
-}
-
-struct LocalGraph<'db, C: IsContext<'db>>
-where
-    [(); <C::Scheme>::N]:,
-{
-    map: Map<C::Scheme, usize>,
-}
-
-impl<'db, C: IsContext<'db>> LocalGraph<'db, C>
-where
-    [(); <C::Scheme>::N]:,
-{
-    // This function triggers the ICE
-    fn new(ctx: C, x: &'db ()) -> Self {
         Self {
-            map: Map::new(ctx, x),
+            _data: unsafe { std::mem::zeroed() },
         }
     }
+}
+
+fn trigger_ice<'db, C: IsContext<'db>>(ctx: C) -> Map<C::Scheme>
+where
+    [(); <C::Scheme>::N]:,
+{
+    Map::new(ctx)
 }
